8fadb41a7d5dc5e55a28238b693b3fa270d66c60
Łukasz Szymkowiak <ryzy82@gmail.com>
2020-01-10 19:44:24 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link :to="{name: 'home'}">Home</router-link> |
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<style src="./assets/tailwind.css">
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    theme: {
+        extend: {
+            height: {
+                '9': '2.375rem',
+                '14': '3.5rem'
+            },
+            width: {
+                'sm': '300px',
+                'md': '600px',
+                'lg': '900px',
+                'xl': '1200px',
+            },
+            screens: {
+                'sm': '600px',
+                'md': '900px',
+                'lg': '1200px',
+                'xl': '1500px',
+            },
+        }
+    },
+    variants: {},
+    plugins: []
+};
