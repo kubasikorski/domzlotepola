88705500be3464ac68606684866b0aa89bd1bfe5
kubasikorski <poczta@kubasikorski.pl>
2020-01-27 22:54:52 +0100

    contact gmap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import VueRellax from 'vue-rellax'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 Vue.use(VueRellax);
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -56,54 +56,53 @@
                 </div>
             </div>
         </div>
-
-        <div class="h-80 bg-primary-lightest my-8 lg:mt-16 relative z-10">
-            <!-- mapa -->
-        </div>
-
         <div v-rellax="{speed: 5, center: true}" class="leaf"></div>
-
         <div class="container relative z-10 lg:px-16">
-            <div class="flex flex-col mt-16 lg:flex-row lg:mt-32">
-                <div class="lg:w-1/2 text-center">
-                    <div class="image-shadow top-left">
-                        <img src="../assets/images/home-references.jpg" alt="" class="">
-                    </div>
-                </div>
+        <div class="h-80 bg-primary-lightest my-8 lg:mt-16 relative z-10">
+            <GmapMap
+                    :center="center"
+                    :zoom="16"
+                    map-type-id="terrain"
+                    style="width: 100%; height: 100%">
+                <GmapMarker
+                        :key="index"
+                        v-for="(m, index) in markers"
+                        :position="m.position"
+                        :clickable="true"
+                        :draggable="false"
+                />
 
-                <div class="text px-16 lg:w-1/2 lg:px-24">
-                    <vue-aos animation-class="fadeInRight animated">
-                        <h2>Referencje</h2>
-                    </vue-aos>
-                    <p>
-                        Co za miejsce, co za Ludzie to prowadzą i JAK! Z Miłością!!! Przepyszne, cudownie smacznie
-                        przyrządzone jedzenie i ładnie podane, w dodatku duże porcje.Menu bogate, byłam bardzo
-                        zaskoczona, że MOŻNA!!! A jadam w Trójmieście, mam porównanie. Wysoka jakość połączona z
-                        pomysłem na miejsce, w którym wszyscy dobrze się czują,( bo tę energię WIDAĆ) i goście, i
-                        obsługa. Obiad był niezwykłą przyjemnością, tak bym właśnie przyrządziła mięso i krem z
-                        pomidorów !! Tatar z łososia bajeczny! kawa wyjątkowo pyszna, mleko bez laktozy jest, smaczne
-                        delikatne wino, piękne szkło, świeże kwiaty w pięknej kompozycji...200 proc. satysfakcji :)
-                        PASJA-oto co oferują tam gościom :) I love it!!!
-                    </p>
-                    <p class="read-more">
-                        <a href="#">Czytaj więcej</a>
-                    </p>
-                </div>
-            </div>
+            </GmapMap>
         </div>
-
-
+        </div>
     </div>
 </template>
 
 <script>
     import Banner from "../components/Banner";
+    import Vue from 'vue'
     import VueAos from "vue-aos";
-
+    import * as VueGoogleMaps from 'vue2-google-maps'
+    Vue.use(VueGoogleMaps, {
+        load: {
+            key: 'AIzaSyC5Bm-E5NjFSkRW6w9jkQkv9i4VNDQF1Ko',
+        },
+    });
     export default {
         components: {
             Banner,
             VueAos
-        }
+        },
+        data() {
+            return {
+                center:{lat:54.376689 , lng:18.579171},
+                markers: [
+                    {
+                        position: {lat: 54.376689, lng: 18.579171},
+                        infoText: '<strong>Marker 1</strong>'
+                    }
+                ]
+            }
+        },
     }
 </script>
