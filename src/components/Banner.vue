b238778df216ce4da22a8bf545484521b582cfd5
kubasikorski <kuba.sikorski@gmail.com>
2024-02-16 13:37:13 +0100

    home banner image

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,8 +4,8 @@
             <slot />
         </div>
       <div v-if="size == 'large'" class="absolute -bottom-16 right-0 flex gap-2">
-        <img class="h-24" src="https://wedding.pl/img/banners/bride_choice2023.png" alt="Wedding.pl Wybór Par Młodych 2023">
         <img class="h-24" src="@/assets/images/Sticker-Overlay-TRA-2022.png">
+        <img class="h-24" src="https://wedding.pl/img/banners/bride_choice2023.png" alt="Wedding.pl Wybór Par Młodych 2023">
       </div>
     </div>
 </template>
