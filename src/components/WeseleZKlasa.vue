0250b584627af92f00d93af805ee61188510cf68
Robert Sikorski <r.sikorski@etos.pl>
2024-07-11 13:52:05 +0200

    wesele z klasa widget

---
+++ src/components/WeseleZKlasa.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container relative z-10 lg:px-16 mb-16 text-sm p-4">
+    <div style="overflow:hidden;"
+         class="wzk-widget iframe-height" data-wzk-widget-type="type1" data-wzk-notice="48184">
+      <div style="background-color:#F5F6FA;text-align:center;padding:16px;font-size:12px;line-height:12px;"><a
+          class="wzk-accent-color" title="Dóm Złote Pola"
+          href="https://www.weselezklasa.pl/ogloszenia-weselne/dom-zlote-pola,48184/#opinie" rel="nofollow"
+          target="_blank" style="color:currentColor;text-decoration:none;">Dóm Złote Pola</a><img
+          style="margin:8px auto 0;display:block" src="https://widgets.4wzk.pl/dist/img/footer-logo.svg"
+          alt="Wesele z klasą"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'wesele-z-klasa',
+  mounted() {
+    (() => {
+      {
+        const e = e => {
+          const t = document.createElement("iframe");
+          t.src = `https://widgets.4wzk.pl/${+e.dataset.wzkNotice % 1e3}/${e.dataset.wzkNotice}/${e.dataset.wzkWidgetType}.html`, e.prepend(t), t.style.border = "none", t.style.width = "100%", t.style.display = "block", t.style.transition = ".1s ease height"
+        };
+        try {
+          window.wzkWigetLoaded || (window.wzkWigetLoaded = !0, [...document.querySelectorAll(".wzk-widget")].forEach((t => {
+            e(t)
+          })))
+        } catch (e) {
+          console.log(e)
+        }
+        window.addEventListener("message", (e => {
+          try {
+            const t = JSON.parse(e.data);
+            if (t.accentColor) document.querySelector(":root").style.setProperty("--accent-color", t.accentColor), [...document.querySelectorAll(".wzk-accent-color")].forEach((e => {
+              e.style.color = t.accentColor
+            })); else if (t.height) {
+              const e = document.querySelector(`.wzk-widget[data-wzk-widget-type="${t.widgetType}"] iframe`);
+              e.style.height = "0px", e.style.height = t.height
+            }
+          } catch (e) {
+            console.log(e)
+          }
+        }))
+      }
+    })();
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,140 +1,150 @@
 <template>
-    <div>
-        <div class="container relative z-10 lg:px-16">
-            <div class="flex flex-col mt-16 lg:flex-row-reverse lg:mt-32">
-                <div class="lg:w-1/2 text-center mt-8 lg:mt-0">
-                    <div class="image-shadow shadow-lighter top-right">
-                        <image-transition :intervalTimer="4000"
-                                          :feed="imageTransitions.dom"></image-transition>
-                    </div>
-                </div>
-                <div class="text px-16 lg:w-1/2 lg:px-24">
-                    <h2>Dóm Złote Pola</h2>
-                    <p>
-                        Dóm Złote Pola znajduje się w bezpośrednim sąsiedztwie Trójmiasta, przy ul. Złote Pola w
-                        urokliwym Miszewie, obok Banina. To doskonała lokalizacja, do której łatwo dojechać z
-                        Trójmiasta i z Kaszub. Czyste powietrze i sielska atmosfera są idealnym otoczeniem dla
-                        wyjątkowych
-                        okazji rodzinnych jak i spotkań firmowych. Dóm Złote Pola położony jest wśród łąk i ziemi
-                        uprawnych. Do domu należy ogród warzywny z kwiatami i owocami.
-                    </p>
-                    <p class="read-more">
-                        <router-link :to="{name: 'dom'}">Czytaj więcej
-                        </router-link>
-                    </p>
-                </div>
-            </div>
-
-            <div class="flex flex-col mt-16 lg:flex-row lg:mt-32">
-                <vue-aos animation-class="fadeInUp animated faster">
-                    <div class="lg:w-1/2 text-center mt-8 lg:mt-0">
-                        <div class="image-shadow shadow-lightest top-left">
-                            <image-transition :intervalTimer="4000"
-                                              :feed="imageTransitions.przyjecia"></image-transition>
-                        </div>
-                    </div>
-                </vue-aos>
-                <vue-aos animation-class="fadeInUp animated">
-                    <div class="text px-16 lg:w-1/2 lg:px-24">
-
-                        <h2>Przyjęcia okolicznościowe</h2>
-                        <p>
-                            Dóm Złote Pola zaprasza do pięknej, pełnej światła sali, na kameralne, jak i większe
-                            przyjęcia okolicznościowe.<br>
-                            Bankiety, spotkania z rodziną i przyjaciółmi, chrzciny, komunie, urodziny, bale, obiady
-                            ślubne, wesela – jesteśmy do Państwa dyspozycji z naszym doświadczeniem, które łączy
-                            tradycję z nowoczesnością.<br>
-                            <br>
-                            Przez ostatnie 10 lat przygotowaliśmy ponad 2800 przyjęć w restauracji BabyCafe.pl tak
-                            abyście czuli się Państwo u nas swobodnie i komfortowo.<br>
-                            Zapewniamy naszą gościnność, miłą obsługę, rodzinną atmosferę, zapachy i smaki domowych
-                            potraw a także fachowe doradztwo w doborze menu i organizacji przyjęć(...)<br>
-                        </p>
-                        <p class="read-more">
-                            <router-link :to="{name: 'dom-parties'}">Czytaj więcej
-                            </router-link>
-                        </p>
-                    </div>
-                </vue-aos>
-            </div>
+  <div>
+    <div class="container relative z-10 lg:px-16">
+      <div class="flex flex-col mt-16 lg:flex-row-reverse lg:mt-32">
+        <div class="lg:w-1/2 text-center mt-8 lg:mt-0">
+          <div class="image-shadow shadow-lighter top-right">
+            <image-transition :intervalTimer="4000"
+                              :feed="imageTransitions.dom"></image-transition>
+          </div>
         </div>
-
-        <div v-rellax="{speed: 5, center: true}" class="leaf"></div>
-
-        <div class="container relative z-10 lg:px-16">
-            <div class="flex flex-col mt-16 lg:flex-row-reverse lg:mt-32">
-                <vue-aos animation-class="fadeInUp animated">
-                    <div class="lg:w-1/2 text-center mt-8 lg:mt-0">
-                        <div class="image-shadow shadow-lighter top-right">
-                            <image-transition :intervalTimer="4000" :feed="imageTransitions.kids"></image-transition>
-                        </div>
-                    </div>
-                </vue-aos>
-                <vue-aos animation-class="fadeInUp animated faster">
-                    <div class="text px-16 lg:w-1/2 lg:px-24">
-                        <h2>Bawialnia dla dzieci</h2>
-
-                        <p>
-                            To odpoczynek dla Państwa i zabawa dla dzieci, czyli słowem – przepis na udane spotkanie
-                            rodzinne.<br><br>
-                            Najmłodszych gości zapraszamy do zabawy w bawialni BabyCafe.pl i do letniego placu zabaw
-                            na świeżym powietrzu w ogrodzie. Nasza kuchnia oferuje ulubione dziecięce potrawy,
-                            takie jak naleśniki z czekoladą, pierogi z truskawkami, rosół z domowym makaronem,
-                            racuszki z jabłkami, z przepisów Babci Krysi.
-                        </p>
-                        <p class="read-more">
-                            <router-link :to="{name: 'playground'}">Czytaj więcej
-                            </router-link>
-                        </p>
-                    </div>
-                </vue-aos>
-            </div>
-            <references bg="true"></references>
+        <div class="text px-16 lg:w-1/2 lg:px-24">
+          <h2>Dóm Złote Pola</h2>
+          <p>
+            Dóm Złote Pola znajduje się w bezpośrednim sąsiedztwie Trójmiasta, przy ul. Złote Pola w
+            urokliwym Miszewie, obok Banina. To doskonała lokalizacja, do której łatwo dojechać z
+            Trójmiasta i z Kaszub. Czyste powietrze i sielska atmosfera są idealnym otoczeniem dla
+            wyjątkowych
+            okazji rodzinnych jak i spotkań firmowych. Dóm Złote Pola położony jest wśród łąk i ziemi
+            uprawnych. Do domu należy ogród warzywny z kwiatami i owocami.
+          </p>
+          <p class="read-more">
+            <router-link :to="{name: 'dom'}">Czytaj więcej
+            </router-link>
+          </p>
         </div>
-      <div class="container relative z-10 lg:px-16 mb-16 text-sm p-4">
-        Dóm Złote Pola uzyskał Subwencję Finansową w ramach umowy z Polskim Funduszem Rozwoju S.A. <img class="h-3 inline" alt="Polski Fundusze Rozwoju S.A." title="Polski Fundusze Rozwoju S.A." src="@/assets/images/pfr.png">
+      </div>
+
+      <div class="flex flex-col mt-16 lg:flex-row lg:mt-32">
+        <vue-aos animation-class="fadeInUp animated faster">
+          <div class="lg:w-1/2 text-center mt-8 lg:mt-0">
+            <div class="image-shadow shadow-lightest top-left">
+              <image-transition :intervalTimer="4000"
+                                :feed="imageTransitions.przyjecia"></image-transition>
+            </div>
+          </div>
+        </vue-aos>
+        <vue-aos animation-class="fadeInUp animated">
+          <div class="text px-16 lg:w-1/2 lg:px-24">
+
+            <h2>Przyjęcia okolicznościowe</h2>
+            <p>
+              Dóm Złote Pola zaprasza do pięknej, pełnej światła sali, na kameralne, jak i większe
+              przyjęcia okolicznościowe.<br>
+              Bankiety, spotkania z rodziną i przyjaciółmi, chrzciny, komunie, urodziny, bale, obiady
+              ślubne, wesela – jesteśmy do Państwa dyspozycji z naszym doświadczeniem, które łączy
+              tradycję z nowoczesnością.<br>
+              <br>
+              Przez ostatnie 10 lat przygotowaliśmy ponad 2800 przyjęć w restauracji BabyCafe.pl tak
+              abyście czuli się Państwo u nas swobodnie i komfortowo.<br>
+              Zapewniamy naszą gościnność, miłą obsługę, rodzinną atmosferę, zapachy i smaki domowych
+              potraw a także fachowe doradztwo w doborze menu i organizacji przyjęć(...)<br>
+            </p>
+            <p class="read-more">
+              <router-link :to="{name: 'dom-parties'}">Czytaj więcej
+              </router-link>
+            </p>
+          </div>
+        </vue-aos>
       </div>
     </div>
+
+    <div v-rellax="{speed: 5, center: true}" class="leaf"></div>
+
+    <div class="container relative z-10 lg:px-16">
+      <div class="flex flex-col mt-16 lg:flex-row-reverse lg:mt-32">
+        <vue-aos animation-class="fadeInUp animated">
+          <div class="lg:w-1/2 text-center mt-8 lg:mt-0">
+            <div class="image-shadow shadow-lighter top-right">
+              <image-transition :intervalTimer="4000" :feed="imageTransitions.kids"></image-transition>
+            </div>
+          </div>
+        </vue-aos>
+        <vue-aos animation-class="fadeInUp animated faster">
+          <div class="text px-16 lg:w-1/2 lg:px-24">
+            <h2>Bawialnia dla dzieci</h2>
+
+            <p>
+              To odpoczynek dla Państwa i zabawa dla dzieci, czyli słowem – przepis na udane spotkanie
+              rodzinne.<br><br>
+              Najmłodszych gości zapraszamy do zabawy w bawialni BabyCafe.pl i do letniego placu zabaw
+              na świeżym powietrzu w ogrodzie. Nasza kuchnia oferuje ulubione dziecięce potrawy,
+              takie jak naleśniki z czekoladą, pierogi z truskawkami, rosół z domowym makaronem,
+              racuszki z jabłkami, z przepisów Babci Krysi.
+            </p>
+            <p class="read-more">
+              <router-link :to="{name: 'playground'}">Czytaj więcej
+              </router-link>
+            </p>
+          </div>
+        </vue-aos>
+      </div>
+      <references bg="true"></references>
+    </div>
+
+    <wesele-z-klasa />
+
+    <div class="container relative z-10 lg:px-16 mb-16 text-sm p-4">
+      Dóm Złote Pola uzyskał Subwencję Finansową w ramach umowy z Polskim Funduszem Rozwoju S.A. <img class="h-3 inline"
+                                                                                                      alt="Polski Fundusze Rozwoju S.A."
+                                                                                                      title="Polski Fundusze Rozwoju S.A."
+                                                                                                      src="@/assets/images/pfr.png">
+    </div>
+  </div>
 </template>
 
 <script>
-    import VueAos from 'vue-aos'
-    import ImageTransition from "../components/ImageTransition";
-    import References from "../components/elements/References";
-    export default {
-        name: 'home',
-        metaInfo: {
-            title: 'Dóm Złote Pola',
-            titleTemplate: '%s - Strona główna',
-            htmlAttrs: {
-                lang: 'pl',
-                amp: true
-            }
-        },
-        components: {
-            VueAos,
-            ImageTransition,
-            References
-        },
-        data: () => ({
-            imageTransitions: {
-                'przyjecia': [
-                    require('@/assets/images/home/home2.jpg'),
-                    require('@/assets/images/home/home2b.jpg'),
-                    require('@/assets/images/home/home2c.jpg'),
-                    require('@/assets/images/home/przyjecia1.jpg'),
-                ],
-                'kids': [
-                    require('@/assets/images/playground/ori1.jpg'),
-                    require('@/assets/images/playground/ori2.jpg'),
-                    require('@/assets/images/playground/kids1.jpg'),
-                    require('@/assets/images/playground/kids2.jpg'),
-                ],
-                'dom': [
-                    require('@/assets/images/home/home0ori.jpg'),
-                    require('@/assets/images/home/home1.jpg'),
-                ]
-            }
-        }),
+import VueAos from 'vue-aos'
+import ImageTransition from "../components/ImageTransition";
+import WeseleZKlasa from "../components/WeseleZKlasa";
+import References from "../components/elements/References";
+
+export default {
+  name: 'home',
+  metaInfo: {
+    title: 'Dóm Złote Pola',
+    titleTemplate: '%s - Strona główna',
+    htmlAttrs: {
+      lang: 'pl',
+      amp: true
     }
+  },
+  components: {
+    WeseleZKlasa,
+    VueAos,
+    ImageTransition,
+    References
+  },
+  data: () => ({
+    imageTransitions: {
+      'przyjecia': [
+        require('@/assets/images/home/home2.jpg'),
+        require('@/assets/images/home/home2b.jpg'),
+        require('@/assets/images/home/home2c.jpg'),
+        require('@/assets/images/home/przyjecia1.jpg'),
+      ],
+      'kids': [
+        require('@/assets/images/playground/ori1.jpg'),
+        require('@/assets/images/playground/ori2.jpg'),
+        require('@/assets/images/playground/kids1.jpg'),
+        require('@/assets/images/playground/kids2.jpg'),
+      ],
+      'dom': [
+        require('@/assets/images/home/home0ori.jpg'),
+        require('@/assets/images/home/home1.jpg'),
+      ]
+    }
+  }),
+
+}
 </script>
